fc0bd5c46f9783e05f4b4f5b53a36c7bf5b7090a
llytaii <llytaii@pop-tower.localdomain>
2021-07-24 17:27:51 +0200

    add VGA buffer Writer with color and basic write! formatting support

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,21 @@
-
-#![no_std]      // tell the compiler not to link the std since we run on bare metal
-#![no_main]     // usually execution of a normal Rust program first starts "crt0" ("C runtime zero"), which e.g. creates a stack.
-                // This then invokes the Rust runtime which is marked by the start language item. This runtime finally calls the main function
-                // here is no runtime so we cant use main as entry point
+#![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
 
-#[no_mangle]    // dont mangle the name of this function
+#[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    use core::fmt::Write;
 
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for(i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::WRITER.lock().write_str("Hello again");
+    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337);
 
     loop {}
 }
 
-
-// this function is called on panic
 #[panic_handler]
-fn panic (_info: &PanicInfo) -> ! {
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
